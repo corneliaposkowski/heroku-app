ff7580efc2c584658b29ea9c6e5c9ffc70392a24
corneliaposkowski <cornelia.poskowski@europace.de>
2019-07-12 15:06:33 +0200

    Erstellen eines Formulas mit Abfrage und Antwort

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template, request
+from random import randrange
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-    #jede route braucht eigene Methode(=def)#
+   return 'Hello World!'
 
-@app.route('/goodbye')
-def goddbye():
-    return 'Goddbye World!'
+
+@app.route('/form')
+def form():
+   return render_template('form.html', name="Donald")
+
+
+@app.route('/result', methods=['get', 'post'] )
+def result():
+
+   firstname = request.form['firstname']
+   lastname = request.form['lastname']
+   place = request.form['place']
+   print("Firstname = " + firstname, "place = " + place)
+   return "Thank you! " + firstname + " in " + place
